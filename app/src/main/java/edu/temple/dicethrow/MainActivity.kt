14e57ce125a58d0d6faebea92cbd4e90702f631e
joue-zero <elnagaryoussef864@gmail.com>
2024-10-17 15:18:04 -0400

    check if the fragment exists or not ?

--- app/src/main/java/edu/temple/dicethrow/MainActivity.kt
+++ app/src/main/java/edu/temple/dicethrow/MainActivity.kt
@@ -13,8 +13,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         findViewById<Button>(R.id.rollDiceButton).setOnClickListener {
             // so the findFragmentById returns a Fragment, we need to cast it to DieFragment
-            val fragment = supportFragmentManager.findFragmentById(R.id.dieContainer) as DieFragment
-            fragment.throwDie()
+//            val fragment = supportFragmentManager.findFragmentById(R.id.dieContainer) as DieFragment
+//            fragment.throwDie()
+            if(supportFragmentManager.findFragmentById(R.id.dieContainer) !is DieFragment) {
+      supportFragmentManager.beginTransaction()
+                    .add(R.id.dieContainer, DieFragment.newInstance(20))
+                    .commit()
+            }
+//            if(savedInstanceState == null)
         }
     }
 }

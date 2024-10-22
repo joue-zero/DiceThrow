8d1ba07175a049b4bc57d0a09564d09de30874da
joue-zero <elnagaryoussef864@gmail.com>
2024-10-22 15:31:29 -0400

    State Management

--- app/src/main/java/edu/temple/dicethrow/MainActivity.kt
+++ app/src/main/java/edu/temple/dicethrow/MainActivity.kt
@@ -11,18 +11,6 @@ class MainActivity : AppCompatActivity(), ButtonInterface {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-            // so the findFragmentById returns a Fragment, we need to cast it to DieFragment
-//            val fragment = supportFragmentManager.findFragmentById(R.id.dieContainer) as DieFragment
-//            fragment.throwDie()
-//            if(supportFragmentManager.findFragmentById(R.id.dieContainer) !is DieFragment) {
-//                supportFragmentManager.beginTransaction()
-//                .add(R.id.dieContainer, DieFragment.newInstance(20))
-//                .commit()
-//            }
-//            else {
-//                val fragment = supportFragmentManager.findFragmentById(R.id.dieContainer) as DieFragment
-//                fragment.throwDie()
-//            }
     }
     override fun buttonClick(){
         if(supportFragmentManager.findFragmentById(R.id.dieContainer) !is DieFragment) {

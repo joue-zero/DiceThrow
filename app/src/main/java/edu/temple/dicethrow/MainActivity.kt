856102bdf42263afbda8c3cb7b73601b992e8107
joue-zero <elnagaryoussef864@gmail.com>
2024-10-22 15:23:28 -0400

    State Management

--- app/src/main/java/edu/temple/dicethrow/MainActivity.kt
+++ app/src/main/java/edu/temple/dicethrow/MainActivity.kt
@@ -7,24 +7,32 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), ButtonInterface {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        findViewById<Button>(R.id.rollDiceButton).setOnClickListener {
             // so the findFragmentById returns a Fragment, we need to cast it to DieFragment
 //            val fragment = supportFragmentManager.findFragmentById(R.id.dieContainer) as DieFragment
 //            fragment.throwDie()
-            if(supportFragmentManager.findFragmentById(R.id.dieContainer) !is DieFragment) {
-                supportFragmentManager.beginTransaction()
+//            if(supportFragmentManager.findFragmentById(R.id.dieContainer) !is DieFragment) {
+//                supportFragmentManager.beginTransaction()
+//                .add(R.id.dieContainer, DieFragment.newInstance(20))
+//                .commit()
+//            }
+//            else {
+//                val fragment = supportFragmentManager.findFragmentById(R.id.dieContainer) as DieFragment
+//                fragment.throwDie()
+//            }
+    }
+    override fun buttonClick(){
+        if(supportFragmentManager.findFragmentById(R.id.dieContainer) !is DieFragment) {
+            supportFragmentManager.beginTransaction()
                 .add(R.id.dieContainer, DieFragment.newInstance(20))
                 .commit()
-            }
-            else {
-                val fragment = supportFragmentManager.findFragmentById(R.id.dieContainer) as DieFragment
-                fragment.throwDie()
-            }
-//            if(savedInstanceState == null)
+        }
+        else {
+            val fragment = supportFragmentManager.findFragmentById(R.id.dieContainer) as DieFragment
+            fragment.throwDie()
         }
     }
 }
